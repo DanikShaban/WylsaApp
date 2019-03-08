af37df02c6ce0ec551e8036afc2048ca9022057c
Shaban Daniil <ludovicthefirst@gmail.com>
2019-03-08 22:21:41 +0300

    Bll

--- App.js
+++ App.js
@@ -1,23 +1,37 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Button, Text, View, StyleSheet } from 'react-native';
+import { Constants, WebBrowser } from 'expo';
 
-export default class App extends React.Component {
+export default class App extends Component {
+  state = {
+    result: null,
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Button
+          style={styles.paragraph}
+          title="Open Something"
+          onPress={this._handlePressButtonAsync}
+        />
+        <Text>{this.state.result && JSON.stringify(this.state.result)}</Text>
       </View>
     );
   }
-}
 
-//test
+  _handlePressButtonAsync = async () => {
+    let result = await WebBrowser.openBrowserAsync('https://classroom.google.com/c/MjI2NzUwMTYwNzJa/a/MjgzMjM1OTQ3NjRa/details');
+    this.setState({ result });
+  };
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
   },
 });
