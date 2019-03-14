ac176dd95b8481c2cfaca30f7794fd3ea192cfdf
Shaban Daniil <ludovicthefirst@gmail.com>
2019-03-14 23:44:36 +0300

    SucceedTimer

--- App.js
+++ App.js
@@ -2,47 +2,17 @@ import React, { Component } from 'react';
 import { Button, Text, View, StyleSheet } from 'react-native';
 import { Constants, WebBrowser } from 'expo';
 
-const INTERVAL = 100;
+const INTERVAL = 1000;
 
-
-export default class Appr extends Component {
-  render () {
-    return (
-      <View>
-          <Timer starttime = {new Date().getTime()}/>
-      </View>
-    );
-  }
-}
 
 class Timer extends Component {
   constructor (props) {
     super (props);
     this.state = {timenow: new Date().getTime(),}
   }
-  render () {
-    let distance = this.state.timenow - this.props.starttime;
-    return (
-      <View style = {styles.forall}>
-      <View style  = {styles.box}>
-        <Text style = {styles.foreach}>Timer:</Text>
-        <Text style = {styles.foreach}>{Math.floor(distance/INTERVAL/60/60%60)} : </Text>
-        <Text style = {styles.foreach}>{Math.floor(distance/INTERVAL/60%60)} : </Text>
-        <Text style = {styles.foreach}>{Math.floor(distance/INTERVAL%60)} . </Text>
-        <Text style = {styles.foreach}>{distance % INTERVAL}</Text>
-        </View>
-      </View>
-    );
-};
-
-/* class Timer extends Component {
-  constructor (props) {
-    super (props);
-    this.state = {value: 0,}
-  }
 
   increment () {
-    this.setState({value: this.state.value + 1});
+    this.setState({timenow:  new Date().getTime()});
   }
 
   componentDidMount() {
@@ -54,20 +24,31 @@ class Timer extends Component {
     }
 
   render () {
-    const value = this.state.value
+    let distance = this.state.timenow - this.props.starttime;
     return (
-      <View style = {styles.forall}>
+      <View >
       <View style  = {styles.box}>
         <Text style = {styles.foreach}>Timer:</Text>
-        <Text style = {styles.foreach}>{Math.floor(value/INTERVAL/60/60%60)} : </Text>
-        <Text style = {styles.foreach}>{Math.floor(value/INTERVAL/60%60)} : </Text>
-        <Text style = {styles.foreach}>{Math.floor(value/INTERVAL%60)} . </Text>
-        <Text style = {styles.foreach}>{value % INTERVAL}</Text>
+        <Text style = {styles.foreach}>{Math.floor(distance/INTERVAL/60/60%60)} : </Text>
+        <Text style = {styles.foreach}>{Math.floor(distance/INTERVAL/60%60)} : </Text>
+        <Text style = {styles.foreach}>{Math.floor(distance/INTERVAL%60)} : </Text>
+        <Text style = {styles.foreach}>{Math.floor((distance/10)%100)}</Text>
         </View>
       </View>
     );
   }
-}*/
+}
+
+export default class App extends Component {
+  render () {
+    return (
+      <View style = {styles.forall}>
+          <Timer starttime = {new Date().getTime()}/>
+      </View>
+    );
+  }
+}
+
 
 const styles = StyleSheet.create({
   forall: {
